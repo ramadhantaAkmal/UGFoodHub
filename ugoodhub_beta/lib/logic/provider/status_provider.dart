26d1638412b073ea139002eaab7420c0787407b6
ramadhantaAkmal <ramadhanta0412@gmail.com>
2022-07-27 13:50:59 +0700

    feat: add new json
    adding new json file in assets: dummy_status.json

---
+++ ugoodhub_beta/lib/logic/provider/status_provider.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StatusProvider extends ChangeNotifier {
+  /*
+  String statusid;
+  int restaurantid;
+  int productid;
+  String productname;
+  status: konfirmasi, disiapkan, diantarkan, selesai
+  */
+
+}
