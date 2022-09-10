63e19d33b8536fabbe41f512ad3b5c168cf8fe67
ramadhantaAkmal <ramadhanta0412@gmail.com>
2022-09-10 15:28:38 +0700

    Update status_provider.dart

--- ugoodhub_beta/lib/logic/provider/status_provider.dart
+++ ugoodhub_beta/lib/logic/provider/status_provider.dart
@@ -4,12 +4,6 @@ import '../../model/status_model.dart';
 import '../../utility/status_api.dart';
 
 class StatusProvider {
-  //status: konfirmasi, disiapkan, diantarkan, selesai
-
-  // StreamController<StatusModel> _controller =
-  //     StreamController<StatusModel>.broadcast();
-  // Stream<StatusModel> get out => _controller.stream;
-
   Stream<List<StatusModel>> loadStream() async* {
     while (true) {
       await Future<void>.delayed(const Duration(seconds: 3));
